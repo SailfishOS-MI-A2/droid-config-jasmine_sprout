056a677f639b7ee0986a0f381cecec55a3296902
BirdZhang <0312birdzhang@gmail.com>
2020-11-30 05:43:30 +0000

    Fix android_storage link script
    Change-Id: I8c0cca64baed302afa0472fca0b97c969b3a097f

--- sparse/usr/bin/droid/android-links.sh
+++ sparse/usr/bin/droid/android-links.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-if [ ! -L /home/nemo/android_storage ]; then
-    ln -s /data/media/0 /home/nemo/android_storage
+NEMOUSER=nemo
+id $NEMOUSER
+if [ "$?" -ne "0" ];then
+    NEMOUSER=defaultuser
+fi
+if [ ! -L /home/$NEMOUSER/android_storage ]; then
+    if [ ! -d /data/media/0 ]; then
+        ln -s /data/media /home/$NEMOUSER/android_storage
+    else
+        ln -s /data/media/0 /home/$NEMOUSER/android_storage
+    fi
 fi
  
